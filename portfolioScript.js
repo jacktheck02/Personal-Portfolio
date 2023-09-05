842f26e540a8c2200fe7dcc55366bd4a012879a0
jhecken2 <94070865+jhecken2@users.noreply.github.com>
2023-09-04 22:03:39 -0700

    Add files via upload

--- portfolioScript.js
+++ portfolioScript.js
@@ -1,24 +1,3 @@
-/*============================= scroll sections active link ================================*/
-let sections = document.querySelector('section');
-let navLinks = document.querySelector('header nav a');
-
-window.onscroll = () => {
-  sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
-
-    if(top >= offset && top < offset + height) {
-        navLinks.forEach(links => {
-            links.classList.remove('active');
-            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-        });
-    }
-  });
-}
-
-
 /*============================= Slideshow ================================*/
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -46,3 +25,9 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+/*============================= Scroll Padding ================================*/
+const navigationHeight = document.querySelector('.header').offsetHeight + document.querySelector('.projects-header').offsetHeight;
+
+document.documentElement.style.setProperty('--scroll-padding', navigationHeight - 1 + "px");
+
